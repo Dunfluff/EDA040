56a749d7fc185177b026d4b3bc9d77c46164e8bc
Lars Korduner <dat14lko@beta-6.student.lth.se>
2016-09-19 13:05:16 +0200

    added stuff

--- alarmclock/src/todo/BThread.java
+++ alarmclock/src/todo/BThread.java
@@ -9,33 +9,34 @@ public class BThread extends Thread {
 	private Semaphore inputSemaphore;
 	private Manager man;
 	private int setTime = -1;
+	private int oldChoice;
+	private int choice;
 
 	public BThread(ClockInput i, Manager m) {
 		input = i;
 		inputSemaphore = input.getSemaphoreInstance();
 		man = m;
+
 	}
 
 	@Override
 	public void run() {
+
 		while (man.isAlive()) {
 			inputSemaphore.take();
-			switch (input.getChoice()) {
+			choice = input.getChoice();
+			switch (oldChoice) {
 			case ClockInput.SET_TIME:
-				setTime = input.getValue();
+				man.setTime(input.getValue());
 				break;
 			case ClockInput.SET_ALARM:
 				man.setAlarm(input.getValue());
-				setTime = -1;
 				break;
 			case ClockInput.SHOW_TIME:
-				if (setTime > -1) {
-					man.setTime(setTime);
-					setTime = -1;
-				}
-				man.disableAlarm();
 				break;
 			}
+			man.disableAlarm();
+			oldChoice = choice;
 		}
 	}
 }
